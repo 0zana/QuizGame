cba543dd8894810b3743f594ea0637ef930ffd68
Joanna <99652750+0zana@users.noreply.github.com>
2022-06-30 13:24:49 +0200

    Update Uzytkownik.java

---
+++ Uzytkownik.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Uzytkownik {
+	public static String wyborUzytkownika(){
+		Scanner scanner = new Scanner(System.in);
+		String odpowiedzUzytkownika = scanner.nextLine();
+		return odpowiedzUzytkownika;
+	}
+}
